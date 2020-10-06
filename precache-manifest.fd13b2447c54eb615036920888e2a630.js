aa0c00a5824c519ab89c5f5e8dc3d84b5176ca77
aliza <lakhani.aliza14@gmail.com>
2020-10-06 17:58:14 +0500

    Updates

---
+++ precache-manifest.fd13b2447c54eb615036920888e2a630.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc049df204e67eff80f6b9e150a11cf",
+    "url": "/Jira-clone/index.html"
+  },
+  {
+    "revision": "04b34dabe6e4e53b21de",
+    "url": "/Jira-clone/static/css/2.ed48ee09.chunk.css"
+  },
+  {
+    "revision": "03ef49aa17af2c8fcaed",
+    "url": "/Jira-clone/static/css/main.0e74f5ee.chunk.css"
+  },
+  {
+    "revision": "04b34dabe6e4e53b21de",
+    "url": "/Jira-clone/static/js/2.92006418.chunk.js"
+  },
+  {
+    "revision": "6d5e94c5d97aed00155d24a6d414f441",
+    "url": "/Jira-clone/static/js/2.92006418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "03ef49aa17af2c8fcaed",
+    "url": "/Jira-clone/static/js/main.644b8fa7.chunk.js"
+  },
+  {
+    "revision": "ee49f0358cf4ea342497",
+    "url": "/Jira-clone/static/js/runtime-main.bef14b82.js"
+  },
+  {
+    "revision": "06afe23e019f2a8b7fb98b1223c375a3",
+    "url": "/Jira-clone/static/media/img.06afe23e.PNG"
+  },
+  {
+    "revision": "65e5689e95be68740e30dc67ac9e5aff",
+    "url": "/Jira-clone/static/media/jira-logo.65e5689e.PNG"
+  }
+]);
